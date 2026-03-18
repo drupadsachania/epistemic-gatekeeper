7862eabb48c4b20aaad98ecd0f6315fde7add10d
Drupad Sachania <drupad.h.sachania@live.com>
2026-03-18 17:12:29 +0530

    Remove comment about custom routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <<BrowserRouter basename="/epistemic-gatekeeper">
+      <BrowserRouter basename="/epistemic-gatekeeper">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
