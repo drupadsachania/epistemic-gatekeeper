10ae9b0414927a2652b7e248a622224b72cde17b
Drupad Sachania <drupad.h.sachania@live.com>
2026-03-19 15:01:10 +0530

    Change BrowserRouter basename for routing

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/epistemic-gatekeeper">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
